5909ed2589a4279ab69a7b36da2883d6bac1bf70
joice-silva <joice.jss2016@gmail.com>
2021-05-04 03:23:32 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,56 @@
+/*
+
+Comentários: com os simbolos // e
+console.log(mensagem) imprime uma mensagem no terminal
+const declara variáveis com valores constantes
+let declara variáveis com valores que podem ser alterados
+Nomeamos variáveis ultilizando o camelCase
+
+console.log("Hello Wolrd")
+
+const nome = prompt("Qual é o seu nome, usuario querido?")
+const idade = prompt("Qual é a sua idade, usuario querido?")
+prompt() solicita informação do usuario
+console.log("Ola", nome, "! Sua idade é", idade)
+
+
+
+*/                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                
+
+/*
+
+let nome = "Joice"
+console.log(nome)
+
+let idade = 28
+console.log(idade)
+
+let maiorDeIdade = true
+console.log(maiorDeIdade)
+
+*/
+
+/*
+
+let idadeUsuario = prompt("Qual sua idade?")
+let idadeAmigoDoUsuario = prompt("Qual a idade do seu brother?")
+
+let soma = idadeUsuario + idadeAmigoDoUsuario
+console.log(soma)
+
+*/
+/*
+
+1. Crie uma variável e atribua seu primeiro nome
+2. Crie uma variável e atribua seu sobrenome
+3. Crie uma variável e atribua sua idade
+4. Crie uma variável que diga se você é ou não é estudante
+5. Imprima o seu nome, sobrenome, idaden e status de estudante no console
+
+*/
+
+const nome = "Joice"
+const sobrenome = "Socorro"
+let idade = 28
+let estudante = true
+console.log("Oi! Meu nome é", nome, sobrenome, ", tenho", idade, "anos. Sou estudante?", estudante)
